71d8ae3edade399fc8344290b20c8e114a10b9fe
greg1002 <gergelychikan@gmail.com>
2018-10-19 22:40:42 -0500

    changed some shit

--- user.php
+++ user.php
@@ -1,1 +1,17 @@
-echo "hello";
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>TopSpot - Log In</title>
+	</head>
+	<body>
+		<link rel="stylesheet" type="text/css" href="Main.css">
+		<div style = "position: relative; margin-top: 100px" class="form-style">
+			<a href="Guest.html"><input type="button" value="Reserve"></a>
+			<a href="Host.html"><input type="button" value="Host"></a>
+			<a href="MyReservations.html"><input type="button" value="My Reservations"></a>
+			<a href="MySpots.html"><input type="button" value="My Spots"></a>
+			<a href="logout.php"><input type="button" value="Log Out"></a>
+		<div>
+		<h6>&copy; 2018 3Faggots + Greg Inc.</h6>
+	</body>
+</html>
