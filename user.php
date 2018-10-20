0b14251f1f46f6295aa8fd6b463cdc840c6ad0b0
greg1002 <gergelychikan@gmail.com>
2018-10-20 16:22:49 -0500

    Стёпа сука

--- user.php
+++ user.php
@@ -1,3 +1,15 @@
+<?php
+    session_start();
+    if(isset($_SESSION['user_id'])){
+
+    }
+    else{
+	     session_destroy();
+       setcookie("PHPSESSID","",time()-3600,"/");
+       echo "<script>alert('Log back in!');window.location.href='Poll_home.html';</script>";
+     }
+    ?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -5,23 +17,12 @@
 	</head>
 	<body>
 	<link rel="stylesheet" type="text/css" href="Main.css">
-    <?php
-    session_start();
-    /*if(isset($_SESSION['user_id'])){
-
-    }
-    else{
-	     session_destroy();
-       setcookie("PHPSESSID","",time()-3600,"/");
-       echo "<script>alert('Log back in!');window.location.href='Poll_home.html';</script>";
-     }*/
-    ?>
 		<img src="TopspotLogo.jpg" class="image">
 		<div style = "position: relative; margin-top: 50px" class="form-style">
       <?php
       echo "<h3 style='text-align: center; font=Arial, Helvetica, sans-serif'>Username: ".$_SESSION['user_id']."</h3>";
       ?>
-			<a href="Guest.html"><input type="button" value="Reserve"></a>
+			<a href="GPS.html"><input type="button" value="Reserve"></a>
 			<a href="Host.html"><input type="button" value="Host"></a>
 			<a href="MyReservations.html"><input type="button" value="My Reservations"></a>
 			<a href="MySpots.php"><input type="button" value="My Spots"></a>
