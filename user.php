cc20a06b03c2db6556d978292d0234a0f45c8f78
greg1002 <gergelychikan@gmail.com>
2018-10-20 07:32:42 -0500

    fagg

--- user.php
+++ user.php
@@ -25,6 +25,7 @@
 			<a href="Host.html"><input type="button" value="Host"></a>
 			<a href="MyReservations.html"><input type="button" value="My Reservations"></a>
 			<a href="MySpots.html"><input type="button" value="My Spots"></a>
+			<a href="MyProfile.html"><input type="button" value="My Profile"></a>
 			<a href="logout.php"><input type="button" value="Log Out"></a>
 		<div>
 		<h6>&copy; 2018 3Faggots + Greg Inc.</h6>
